25447ac08d3f182fd706cdc05292bf256b9d2999
Weslley Alves <weslleynfs@gmail.com>
2023-11-10 06:10:22 -0300

    add Lato font

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+
 import Login from './src/screens/Login';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Lato': require('./assets/fonts/Lato.ttf'),
+  });
+
+  if (!fontsLoaded) return null
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
